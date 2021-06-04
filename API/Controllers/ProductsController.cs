066f567fbe8c14054c6923c23bf7a24cd211e5c4
Alex Kaziuka <alex.kaziuka@outlook.com>
2021-06-03 22:16:29 -0400

    Generics implementation

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     public class ProductsController : ControllerBase
     {
         private readonly IGenericRepository<Product> _productRepo;
---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    // Constraint this type to only 'Entity' classes (BaseEntity)
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery,
+            ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+            
+            // Evaluate what's inside the specification
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            // Combine all 'include' operations (Aggregate)
+            query = spec.Includes.Aggregate(query, (current, include) =>
+                current.Include(include));
+            
+            return query;
+        }
+    }
+}
