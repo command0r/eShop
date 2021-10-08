2c7121308019f5a718ac9858731f5a7b6e6ce12f
Alex Kaziuka <alex.kaziuka@outlook.com>
2021-10-07 22:51:12 -0400

    Base page composition and getting data from the API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IProduct} from "./models/product";
+import {IPagination} from "./models/pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'E-Commerce';
+  // TypeScript 2.7 and up has strict class checking and the property has to be initialized
+  // or marked as 'not null' (by adding '!'). Note: "products: any[] = []" will work as well
+  products!: IProduct[];
+
+  // Constructor
+  constructor(private http: HttpClient) { }
+
+  // Lifecycle methods
+  ngOnInit(): void {
+    // Gets access to a backend API
+    // we need to 'subscribe' to make use of an 'observable'
+    this.http.get<IPagination>('https://localhost:5001/api/products?PageSize=20').subscribe(
+      (response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
