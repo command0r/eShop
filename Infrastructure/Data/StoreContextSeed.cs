ce44a461c0a9964a5ad3024ac522b0e5759aa725
Alex Kaziuka <akazyuka@gmail.com>
2022-04-13 23:08:54 -0400

    Publishing prep

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
@@ -16,12 +17,15 @@ namespace Infrastructure.Data
         {
             try
             {
+                // Unify base patch for seed files
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
                 // Seed brands data
                 if (!context.ProductBrands.Any())
                 {
-                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json");
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
-                    
+
                     // Add deserialized items into a DB
                     foreach (var item in brands)
                     {
@@ -30,13 +34,13 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
-                
+
                 // Seed product types data
                 if (!context.ProductTypes.Any())
                 {
-                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var typesData = File.ReadAllText(path + @"/Data/SeedData/types.json");
                     var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
-                    
+
                     // Add deserialized items into a DB
                     foreach (var item in types)
                     {
@@ -45,13 +49,13 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
-                
+
                 // Seed products data
                 if (!context.Products.Any())
                 {
-                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
-                    
+
                     // Add deserialized items into a DB
                     foreach (var item in products)
                     {
@@ -60,13 +64,13 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
-                
+
                 // Seed delivery methods data
                 if (!context.DeliveryMethods.Any())
                 {
-                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                    var dmData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
                     var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
-                    
+
                     // Add deserialized items into a DB
                     foreach (var item in methods)
                     {
